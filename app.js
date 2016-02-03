f8dffb4b34ada6cd3eab143dd0a15e3383d6ac6d
Jay Lin <weijingjaylin@gmail.com>
2016-02-02 23:20:51 -0800

    express router issue

--- app.js
+++ app.js
@@ -1,24 +1,51 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var compression = require('compression');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs  = require('express-handlebars');
+var helpers = require('./lib/helpers');
+var app = express();
+var server = http.createServer(app);
+
+app.use(compression());
+// app.use(app.router); don't need in express 4.x
+
+// Create `ExpressHandlebars` instance with a default layout.
+var hbs = exphbs.create({
+    defaultLayout: 'main',
+    helpers      : helpers,
+
+    // Uses multiple partials dirs, templates in "shared/templates/" are shared
+    // with the client-side of the app (see below).
+    partialsDir: [
+        'shared/templates/',
+        'views/partials/'
+    ]
+});
+
 //var reload = require('reload');
 //var sockjs = require('sockjs');
 
+/*
 var routes = require('./routes/index');
 var users = require('./routes/users');
+*/
+require('./routes/index.js');
+var config = require('./config');
 
-var app = express();
-var server = http.createServer(app)
+
 
 // view engine setup
+app.engine('handlebars', hbs.engine);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'handlebars');
 //app.engine('.html', require('jade'));
 //app.listen(80);
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -28,7 +55,44 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Middleware to expose the app's shared templates to the cliet-side of the app
+// for pages which need them.
+function exposeTemplates(req, res, next) {
+    // Uses the `ExpressHandlebars` instance to get the get the **precompiled**
+    // templates which will be shared with the client-side of the app.
+    hbs.getTemplates('shared/templates/', {
+        cache      : app.enabled('view cache'),
+        precompiled: true
+    }).then(function (templates) {
+        // RegExp to remove the ".handlebars" extension from the template names.
+        var extRegex = new RegExp(hbs.extname + '$');
+
+        // Creates an array of templates which are exposed via
+        // `res.locals.templates`.
+        templates = Object.keys(templates).map(function (name) {
+            return {
+                name    : name.replace(extRegex, ''),
+                template: templates[name]
+            };
+        });
+
+        // Exposes the templates during view rendering.
+        if (templates.length) {
+            res.locals.templates = templates;
+        }
+
+        setImmediate(next);
+    })
+    .catch(next);
+}
+
 app.use('/', routes);
+/*
+app.get('/', function (req, res) {
+    res.render('home', {
+        title: 'Home'
+    });
+});*/
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -63,5 +127,5 @@ app.use(function(err, req, res, next) {
 });
 
 //reload(server, app, 5000, 0);
-
+app.use(express.static('public/'));
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
+/*
 var express = require('express');
 var router = express.Router();
-
+var app = express();
+*/
+var app = ('../app.js');
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*
+app.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+*/
 
-module.exports = router;
+app.get('/', function (req, res) {
+    res.render('home', {
+        title: 'Home'
+    });
+});
+
+//module.exports = router;
